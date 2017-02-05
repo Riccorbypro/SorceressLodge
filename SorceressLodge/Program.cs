a298af6f273297ab4a40b74c7906474de9eff7bb
WelterZen <jacb277@gmail.com>
2017-02-05 13:48:46 +0200

    test

--- SorceressLodge/Program.cs
+++ SorceressLodge/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace SorceressLodge {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("FuckButt");
         }
     }
 }
