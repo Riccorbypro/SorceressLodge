ea59e99a2cae10bc0ebbc2b57b5e27702c3136d9
WelterZen <jacb277@gmail.com>
2017-02-05 16:56:56 +0200

    users class added

---
+++ SorceressLodge/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorceressLodge {
+    class User {
+        int uID;
+        double bounty;
+        string description;
+        string name;
+        string surname;
+        string location;
+        Image image;
+
+        public double Bounty {
+            get { return bounty; }
+            set { bounty = value; }
+        }
+
+        public int UID {
+            get { return uID; }
+            set { uID = value; }
+        }
+
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public string Location {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public Image Image {
+            get { return image; }
+            set { image = value; }
+        }
+
+        public User(int uid, double bounty, string surname, string name, string des, string location, Image imge) {
+            this.bounty = bounty;
+            this.description = des;
+            this.location = location;
+            this.name = name;
+            this.surname = surname;
+            this.uID = uid;
+            this.image = imge;
+        }
+
+
+    }
+}
