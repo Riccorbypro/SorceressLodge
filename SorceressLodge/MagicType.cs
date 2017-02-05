29f4d29605b82d672ef4c0a28bf0b08bfe236e94
Richard Corbishley <riccorbypro@gmail.com>
2017-02-05 14:29:40 +0200

    Added Magic Types

---
+++ SorceressLodge/MagicType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorceressLodge {
+    abstract class MagicType {
+        private string typeName;
+
+        public string Type {
+            get { return typeName; }
+            set { typeName = value; }
+        }
+
+        private bool isAllowed;
+
+        public bool Allowed {
+            get { return isAllowed; }
+            set { isAllowed = value; }
+        }
+    }
+}
