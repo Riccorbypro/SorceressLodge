097bb3d1181281032c4a99333ed82c3042a0165f
Jacques <Jacques@DESKTOP-C12M830>
2017-02-22 16:07:58 +0200

    backend changes

--- SorceressLodge/Backend.cs
+++ SorceressLodge/Backend.cs
@@ -17,10 +17,31 @@ namespace SorceressLodge {
         }
 
         public DataGridView SearchUsers(Dictionary<string, object> searchTerms) {
-            DataGridView dgv = new DataGridView();
+
+            string conn = @"Data Source=DESKTOP-C12M830\SQLEXPRESS;Initial Catalog=first;Integrated Security=True";
+
+            sqlConnect = new SqlConnection(conn);
+            sqlConnect.Open();
+
+            string Query1 = "SELECT * from Store_Details";
+
+            sqlCommand = new SqlCommand(Query1, sqlConnect);
+            dataAdapt = new SqlDataAdapter(sqlCommand);
+            dataset = new DataSet();
+            dataAdapt.Fill(dataset, "Store_Details");
+            commBuilder = new SqlCommandBuilder(dataAdapt);
+            dataTable = dataset.Tables["Store_Details"];
+            sqlConnect.Close();
+
+            //MessageBox.Show(dataTable.TableName.ToString());
+            //MessageBox.Show(dataset.DataSetName.ToString());
+
+            dataGridView1.DataSource = dataTable;
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.ReadOnly = true;
 
 
-            return dgv;
         }
 
     }
