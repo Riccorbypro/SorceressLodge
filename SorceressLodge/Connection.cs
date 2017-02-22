da5a4255d05da8874e638d524d96d495b4e3580b
Jacques <Jacques@DESKTOP-C12M830>
2017-02-22 17:41:25 +0200

    Connections changes

--- SorceressLodge/Connection.cs
+++ SorceressLodge/Connection.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Sql;
 using System.Data.SqlClient;
+using System.Drawing;
 
 namespace SorceressLodge {
     class Connection {
@@ -25,16 +26,41 @@ namespace SorceressLodge {
             foreach (object[] oa in osLocation) {
                 location.Add(new Location((int)oa[0], (int)oa[1], (string)oa[2], (DateTime)oa[3]));
             }
+
             List<object[]> osMagicTypes = ReadData("MagicTypes");
             List<MagicType> magictype = new List<MagicType>();
             foreach (object[] oa in osMagicTypes) {
                 magictype.Add(new MagicType((int)oa[0],(string)oa[1],(bool)oa[2]));
             }
-            //List<object[]> os = ReadData("Bounty");
-            //List<Location> location = new List<Location>();
-            //foreach (object[] oa in osLocation) {
-            //    location.Add(new Location((int)oa[0], (int)oa[1], (string)oa[2], (DateTime)oa[3]));
-            //}
+
+            List<object[]> osBounty = ReadData("Bounty");
+            List<Bounty> bounty = new List<Bounty>();
+            foreach (object[] oa in osBounty) {
+                bounty.Add(new Bounty((int)oa[0], (int)oa[1], (double)oa[2]));
+            }
+
+            int uidMU = 0;
+            double[] bountyMU = { 0};
+            string surnameMU = "";
+            string nameMU = "";
+            string descriptionMU = "";
+            List< Location > locationMU = new List<Location>();
+            Image imageMU = null;
+            Dictionary< MagicType, int> skillsMU = new Dictionary<MagicType, int>();
+
+            List<object[]> osMagicUser = ReadData("MagicUsers");
+            foreach (object[] oa in osMagicUser) {
+                MagicUserlst.Add(new MagicUser((int)oa[0], bountyMU, (string)oa[2], (string)oa[1],(string)oa[3],locationMU,(Image)oa[4],skillsMU));
+            }
+
+            foreach (var locationItem in location) {
+                foreach (var muItem in MagicUserlst) {
+                    if (locationItem.UserID.Equals(muItem)) {
+
+                    }
+                }
+            }
+
             return MagicUserlst;
         }
 
